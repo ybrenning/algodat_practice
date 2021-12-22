89142b7198534e79f07b6e7541e202a8f8b24a9d
yznnck <yannickbrenning2@gmail.com>
2021-12-23 00:28:53 +0100

    Fixed merge sort

--- algorithms/sorting/merge_sort/merge_sort.py
+++ algorithms/sorting/merge_sort/merge_sort.py
@@ -5,12 +5,11 @@ def merge(arr, left, mid, right):
     i = j = 0
     k = left
     while i < len(L) and j < len(R):
-        print(k)
         if L[i] <= R[j]:
             arr[k] = L[i]
             i += 1
         elif L[i] > R[j]:
-            arr[k] = L[j]
+            arr[k] = R[j]
             j += 1
         k += 1
 
@@ -18,7 +17,6 @@ def merge(arr, left, mid, right):
         arr[k] = L[i]
         i += 1
         k += 1
-
     while j < len(R):
         arr[k] = R[j]
         j += 1
@@ -27,16 +25,7 @@ def merge(arr, left, mid, right):
 
 def merge_sort(arr, left, right):
     if left < right:
-        mid = (left + right) // 2
+        mid = left + ((right - left) // 2)
         merge_sort(arr, left, mid)
         merge_sort(arr, mid + 1, right)
         merge(arr, left, mid, right)
-
-
-def main():
-    arr = [5, 4, 1, 2, 3]
-    merge_sort(arr, 0, len(arr) - 1)
-    print(arr)
-
-if __name__ == "__main__":
-    main()
