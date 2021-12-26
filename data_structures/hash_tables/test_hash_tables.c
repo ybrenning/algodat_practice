53686e1d44b369e3eb9211ff967a9b95fae35a70
ybrenning <yannickbrenning2@gmail.com>
2021-12-27 00:50:12 +0100

    Added ht_hash() testing

--- data_structures/hash_tables/test_hash_tables.c
+++ data_structures/hash_tables/test_hash_tables.c
@@ -107,7 +107,7 @@ void run_all_tests() {
     test_ht_init();
     test_ht_entry_init();
     test_ht_destroy();
-    //test_ht_hash();
+    test_ht_hash();
     //test_ht_add();
     //test_ht_get();
     //test_ht_key_exists();
