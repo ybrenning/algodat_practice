ffe1e3d1f42b96288ca0a4e6cfdf10ab483f15db
ybrenning <yannickbrenning2@gmail.com>
2021-12-27 00:49:33 +0100

    Added ht_destroy() testing

--- data_structures/hash_tables/test_hash_tables.c
+++ data_structures/hash_tables/test_hash_tables.c
@@ -106,7 +106,7 @@ void test_ht_remove_key() {
 void run_all_tests() {
     test_ht_init();
     test_ht_entry_init();
-    //test_ht_destroy();
+    test_ht_destroy();
     //test_ht_hash();
     //test_ht_add();
     //test_ht_get();
