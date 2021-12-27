0db28e1ec3863995bca0b48bfae370306dcdfc6e
ybrenning <yannickbrenning2@gmail.com>
2021-12-27 18:00:09 +0100

    Updated testing

--- data_structures/graphs/adjacency_list/test_adjacency_list.c
+++ data_structures/graphs/adjacency_list/test_adjacency_list.c
@@ -2,11 +2,25 @@
 
 void test_graph_init() {
     graph_t *graph = graph_init(true, 5);
+    assert(graph->directed);
+    assert(graph->vertices == 5);
+
+    for (int i = 0; i < graph->vertices; i++) {
+        assert(graph->list[i].head == NULL);
+    }
+
     graph_destroy(&graph);
+}
+
+void test_graph_destroy() {
+    graph_t *graph = graph_init(true, 5);
+    graph_destroy(&graph);
+    assert(!graph);
 }
 
 void run_all_tests() {
     test_graph_init();
+    test_graph_destroy();
 }
 
 int main() {
