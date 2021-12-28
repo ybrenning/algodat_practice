14c064db05354c509c171f777f3358fe06a1c345
ybrenning <yannickbrenning2@gmail.com>
2021-12-28 23:33:16 +0100

    Added helper functions

--- data_structures/graphs/adjacency_list/adjacency_list.c
+++ data_structures/graphs/adjacency_list/adjacency_list.c
@@ -30,3 +30,24 @@ void graph_destroy(graph_t **graph) {
     free(*graph);
     *graph = NULL;
 }
+
+bool has_vertex(graph_t *graph, unsigned int vertex) {
+    return vertex >= 0 && vertex < graph->vertices;
+}
+
+bool has_edge(graph_t *graph, unsigned int vertex_1, unsigned int vertex_2) {
+    if (has_vertex(graph, vertex_1) && has_vertex(graph, vertex_2)) {
+        node_t *curr = graph->list[vertex_1].head;
+        for (int i = 0; i < vertex_2; i++) {
+            curr = curr->next;
+        }
+
+        if (curr != NULL) return true;
+    }
+
+    return false;
+}
+
+bool graph_add_edge(graph_t *graph, unsigned int vertex_1, unsigned int vertex_2) {
+    return false;
+}
