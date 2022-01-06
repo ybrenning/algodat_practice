baf8d2f4577129f018ef976f4a73f7bb1e62a75e
ybrenning <yannickbrenning2@gmail.com>
2022-01-06 23:33:10 +0100

    Reformatting

--- data_structures/linked_lists/singly_linked_lists/singly_linked_lists.py
+++ data_structures/linked_lists/singly_linked_lists/singly_linked_lists.py
@@ -6,10 +6,10 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.size = 0
         self.head = None
+        self.size = 0
 
-    def get_at_index(self, index):
+    def get(self, index):
         """
         Returns value of node at index
         :param index: index of node
@@ -23,11 +23,12 @@ class LinkedList:
             curr = self.head
             for _ in range(0, index):
                 curr = curr.next
+
             return curr.val
 
-    def add_at_head(self, val):
+    def push(self, val):
         """
-        Inserts node at the front of the linkedlist
+        Inserts node at the front of the linked list
         :param val: value to be stored in node
         :return: None
         """
@@ -37,24 +38,26 @@ class LinkedList:
             new_node = Node(val)
             new_node.next = self.head
             self.head = new_node
+
         self.size += 1
 
-    def add_at_tail(self, val):
+    def append(self, val):
         """
-        Inserts node at the end of the linkedlist
+        Inserts node at the end of the linked list
         :param val: value to be stored in node
         :return: None
         """
         if self.size == 0:
-            self.add_at_head(val)
+            self.push(val)
         else:
             curr = self.head
             for _ in range(0, self.size - 1):
                 curr = curr.next
             curr.next = Node(val)
+
             self.size += 1
 
-    def add_at_index(self, val, index):
+    def insert(self, val, index):
         """
         Insert new node at index
         :param val: value to be stored in node
@@ -64,20 +67,23 @@ class LinkedList:
         if index < 0 or index > self.size:
             return
         elif index == 0:
-            self.add_at_head(val)
+            self.push(val)
         elif index == self.size:
-            self.add_at_tail(val)
+            self.append(val)
         else:
             curr = self.head
             for _ in range(0, index - 1):
                 curr = curr.next
+
             new_node = Node(val)
+
             temp = curr.next
             curr.next = new_node
             new_node.next = temp
+
             self.size += 1
 
-    def remove_at_index(self, index):
+    def delete(self, index):
         """
         Remove node at index
         :param index: index of node
@@ -96,6 +102,7 @@ class LinkedList:
             for _ in range(0, index - 1):
                 curr = curr.next
             curr.next = curr.next.next
+
         self.size -= 1
 
     def __str__(self):
@@ -104,26 +111,36 @@ class LinkedList:
         while curr is not None:
             output += " " + str(curr.val) + ","
             curr = curr.next
+
         output = output[:-1]
         output += " ]"
+
         return output
 
 
-if __name__ == "__main__":
-    my_llist = LinkedList()
-    my_llist.add_at_head(3)
-    print(my_llist.size)
-    my_llist.add_at_tail(4)
-    print(my_llist.size)
-    my_llist.add_at_tail(50)
-    print(my_llist.size)
-    my_llist.add_at_index(30, 3)
-    print(my_llist.size)
-    print(my_llist)
+def main():
+    linked_list = LinkedList()
+    linked_list.push(3)
+    print(linked_list.size)
 
-    print(my_llist.get_at_index(2))
-    print(my_llist.get_at_index(0))
-    print(my_llist.get_at_index(5))
-    print(my_llist)
-    my_llist.remove_at_index(2)
-    print(my_llist)
+    linked_list.append(4)
+    print(linked_list.size)
+
+    linked_list.append(50)
+    print(linked_list.size)
+
+    linked_list.insert(30, 3)
+    print(linked_list.size)
+    print(linked_list)
+
+    print(linked_list.get(2))
+    print(linked_list.get(0))
+    print(linked_list.get(5))
+    print(linked_list)
+
+    linked_list.delete(2)
+    print(linked_list)
+
+
+if __name__ == "__main__":
+    main()
