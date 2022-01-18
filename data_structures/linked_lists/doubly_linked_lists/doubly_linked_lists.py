0352bbb8e486c24786796bf7880d2ab831a04f4c
ybrenning <yannickbrenning2@gmail.com>
2022-01-19 00:50:34 +0100

    Documentation and reformatting

--- data_structures/linked_lists/doubly_linked_lists/doubly_linked_lists.py
+++ data_structures/linked_lists/doubly_linked_lists/doubly_linked_lists.py
@@ -2,6 +2,7 @@
 Implementation of Doubly Linked Lists.
 """
 
+from __future__ import annotations
 from typing import Any
 
 
@@ -17,23 +18,64 @@ class DoublyLinkedList:
         self.head: Node | None = None
         self.size = 0
 
+    def __str__(self) -> str:
+        output = "[ "
+
+        curr = self.head
+        for _ in range(0, self.size):
+            output += str(curr.val) + " "
+            curr = curr.next
+
+        output += "]"
+
+        return output
+
+    def __len__(self) -> int:
+        return self.size
+
     def is_empty(self) -> bool:
         return self.size == 0
 
     def append(self, val: Any) -> None:
+        """
+        Appends node to end of Doubly Linked List
+        :param val: value of node to append
+        :return: None
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> print(linked_list)
+        [ 1 2 3 ]
+        """
+
         if self.is_empty():
             self.head = Node(val)
         else:
             curr = self.head
             while curr.next is not None:
                 curr = curr.next
+
             new_node = Node(val)
             new_node.prev = curr
 
             curr.next = new_node
+
         self.size += 1
 
     def push(self, val: Any) -> None:
+        """
+        Push node to front of Doubly Linked List
+        :param val: value of node to be added
+        :return: None
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.push(1)
+        >>> linked_list.push(2)
+        >>> linked_list.push(3)
+        >>> print(linked_list)
+        [ 3 2 1 ]
+        """
+
         if self.is_empty():
             self.head = Node(val)
         else:
@@ -47,6 +89,22 @@ class DoublyLinkedList:
         self.size += 1
 
     def insert(self, val: Any, index: int) -> None:
+        """
+        Inserts a new node at a given index
+        :param val: value of node to insert
+        :param index: position at which to insert
+        :return: None
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> linked_list.append(4)
+        >>> linked_list.insert(5, 2)
+        >>> linked_list.insert(6, 1)
+        >>> print(linked_list)
+        [ 1 6 2 5 3 4 ]
+        """
+
         if index < 0 or index > self.size:
             raise IndexError("Index out of bounds")
         elif index == 0:
@@ -66,7 +124,18 @@ class DoublyLinkedList:
 
             self.size += 1
 
-    def remove_last(self) -> Any:
+    def remove_tail(self) -> Any:
+        """
+        Removes the last node
+        :return: value of deleted node
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.remove_last()
+        >>> print(linked_list)
+        [ 1 ]
+        """
+
         if self.is_empty():
             raise IndexError("Removing from empty list")
         elif self.size == 1:
@@ -83,7 +152,17 @@ class DoublyLinkedList:
         self.size -= 1
         return retval
 
-    def pop(self) -> Any:
+    def remove_head(self) -> Any:
+        """
+        Removes the first node
+        :return: value of deleted node
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> print(linked_list)
+        [ 2 ]
+        """
+
         if self.is_empty():
             raise IndexError("Pop from empty list")
         else:
@@ -97,6 +176,20 @@ class DoublyLinkedList:
         return retval
 
     def delete(self, index: int) -> Any:
+        """
+        Remove node at a given index
+        :param index: position of node to delete
+        :return: value of deleted node
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> linked_list.append(4)
+        >>> linked_list.delete(2)
+        >>> print(linked_list)
+        [ 1 2 4 ]
+        """
+
         if index < 0 or index > self.size:
             raise IndexError("Index out of bounds")
         elif index == 0:
@@ -119,6 +212,18 @@ class DoublyLinkedList:
         return retval
 
     def get(self, index: int) -> Any:
+        """
+        Get value of node at given index
+        :param index: position of node to find
+        :return: value of node at index
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> print(linked_list.get(1))
+        2
+        """
+
         if self.is_empty() or index < 0 or index >= self.size:
             raise IndexError("Index out of bounds")
         else:
@@ -129,6 +234,19 @@ class DoublyLinkedList:
             return curr.val
 
     def reverse(self) -> None:
+        """
+        Reverses the Doubly Linked List
+        :return: None
+        >>> linked_list = DoublyLinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> linked_list.append(4)
+        >>> linked_list.reverse()
+        >>> print(linked_list)
+        [ 4 3 2 1 ]
+        """
+
         if not self.size <= 1:
             curr = self.head
             temp = None
@@ -140,20 +258,6 @@ class DoublyLinkedList:
                 curr = curr.prev
 
             self.head = temp.prev
-
-    def __str__(self) -> str:
-        if self.is_empty():
-            return "[ ]"
-        else:
-            output = "[ "
-            curr = self.head
-            for _ in range(0, self.size):
-                output += str(curr.val) + " "
-                curr = curr.next
-
-            output += "]"
-
-        return output
 
 
 def main():
@@ -173,8 +277,8 @@ def main():
 
     linked_list.push(0)
     linked_list.insert(6, 5)
-    linked_list.remove_last()
-    linked_list.pop()
+    linked_list.remove_tail()
+    linked_list.remove_head()
     linked_list.delete(4)
     print(linked_list.get(2))
 
@@ -182,7 +286,8 @@ def main():
 
     print(linked_list)
 
-    # Swaps each prev and next pointer
+
+    # Reverse method: swaps each prev and next pointer
     # using a temp node to store the prev node
 
     # None <- old_head -> <- node_1 -> ... <- temp -> <- old_tail -> None
