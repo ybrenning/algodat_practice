689e05f8fe8d8fe7cf011c2224791a94940344a2
ybrenning <yannickbrenning2@gmail.com>
2022-01-19 20:26:27 +0100

    Add `shortest_path()` with BFS

--- data_structures/graphs/adjacency_list/adjacency_list.py
+++ data_structures/graphs/adjacency_list/adjacency_list.py
@@ -10,10 +10,24 @@ class Node:
 
 
 class Graph:
-    def __init__(self, directed: bool =True, vertices: int =5) -> None:
+    def __init__(self, vertices: int, directed: bool = True) -> None:
         self.directed = directed
         self.vertices = vertices
         self.edges: list[Node | None] = [None] * vertices
+
+    def __str__(self) -> str:
+        output = ""
+        for i in range(0, self.vertices):
+            output += str(i) + ": "
+
+            curr = self.edges[i]
+            while curr is not None:
+                output += str(curr.val) + " "
+                curr = curr.next
+
+            output += "\n"
+
+        return output
 
     def has_vertex(self, vertex: int) -> bool:
         return 0 <= vertex < self.vertices
@@ -24,6 +38,7 @@ class Graph:
             while curr is not None:
                 if curr.val == vertex_2:
                     return True
+
                 curr = curr.next
 
         return False
@@ -35,6 +50,7 @@ class Graph:
         :param vertex_2: target/destination vertex
         :return: bool value representing success or failure
         """
+
         if not self.has_edge(vertex_1, vertex_2):
             node = Node(vertex_2)
             node.next = self.edges[vertex_1]
@@ -56,6 +72,7 @@ class Graph:
         :param vertex_2: value of node within linked list to be deleted
         :return: bool representing success or failure of deletion
         """
+
         curr = self.edges[vertex_1]
         if curr is not None and curr.val == vertex_2:
             self.edges[vertex_1] = self.edges[vertex_1].next
@@ -77,6 +94,7 @@ class Graph:
         :param vertex_2: target/destination vertex
         :return: bool representing success or failure of deletion
         """
+
         if not self.has_edge(vertex_1, vertex_2):
             return False
 
@@ -86,10 +104,12 @@ class Graph:
 
     def dfs(self, vertex: int) -> None:
         """
-        Depth-First-Search and print the graph from a specific vertex
+        Depth-First-Search and print the
+        graph from a specific vertex
         :param vertex: vertex to start from
         :return: None
         """
+
         visited = [False] * self.vertices
         stack = [vertex]
         visited[vertex] = True
@@ -110,10 +130,12 @@ class Graph:
 
     def bfs(self, vertex: int) -> None:
         """
-        Breadth-First-Search and print the graph from a specific vertex
+        Breadth-First-Search and print the
+        graph from a specific vertex
         :param vertex: starting point
         :return: None
         """
+
         visited = [False] * self.vertices
         queue = [vertex]
         visited[vertex] = True
@@ -131,15 +153,46 @@ class Graph:
 
         print("\n")
 
-    def shortest_paths(self, start):
-        path = [None] * self.vertices
-        distance = [-1] * self.vertices
+    def shortest_path(self, start: int, dest: int) -> int:
+        """
+        Find the shortest path between two vertices using BFS
+        :param start: starting/source vertex
+        :param dest: destination/target vertex
+        :return: path distance as int
+        """
+
+        distance: list[int] = [-1] * self.vertices
+        distance[start] = 0
+
+        queue: list[int] = [start]
+        while len(queue) > 0:
+            vertex = queue.pop(0)
+
+            curr_node = self.edges[vertex]
+            while curr_node is not None:
+                if distance[curr_node.val] == -1:
+                    distance[curr_node.val] = distance[vertex] + 1
+                    queue.append(curr_node.val)
+
+                curr_node = curr_node.next
+
+        return distance[dest]
+
+    def shortest_paths(self, start: int) -> None:
+        """
+        Find shortest paths from a starting vertex to all
+        other nodes in the graph and print to console
+        :param start: starting vertex
+        :return: None
+        """
+
+        path: list[int | None] = [None] * self.vertices
+        distance: list[int] = [-1] * self.vertices
 
         path[start] = start
         distance[start] = 0
 
-        queue = [start]
-
+        queue: list[int] = [start]
         while len(queue) > 0:
             size = len(queue)
 
@@ -157,29 +210,11 @@ class Graph:
 
                 size -= 1
 
-            print(f"Distance from node {start} to...")
-            for i in range(0, self.vertices):
-                print(f"{i} is {distance[i]} via path {path[i]}")
-
-            print("\n")
-
-
-
-
-
-    def __str__(self) -> str:
-        output = ""
+        print(f"Distance from node {start} to...")
         for i in range(0, self.vertices):
-            output += str(i) + ": "
+            print(f"{i} is {distance[i]} via path {path[i]}")
 
-            curr = self.edges[i]
-            while curr is not None:
-                output += str(curr.val) + " "
-                curr = curr.next
-
-            output += "\n"
-
-        return output
+        print("\n")
 
 
 def main():
@@ -187,17 +222,23 @@ def main():
     # Driver code to generate the following graph:
     #
     #       0 --> 1, 2, 3
+    #       1 --> 2
+    #       2 --> 4
     #       3 --> 1
     #       4 --> 2, 3
 
-    graph = Graph()
+    graph = Graph(5)
     graph.add_edge(0, 1)
     graph.add_edge(0, 2)
     graph.add_edge(0, 3)
+    graph.add_edge(1, 2)
     graph.add_edge(2, 4)
     graph.add_edge(3, 1)
     graph.add_edge(4, 2)
     graph.add_edge(4, 3)
+
+    print(graph)
+    print(graph.shortest_path(1, 4), "\n")
 
     graph.dfs(0)
     graph.bfs(0)
