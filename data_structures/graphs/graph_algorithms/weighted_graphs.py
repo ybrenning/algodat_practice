0dadffbc5dfa6732510d8d10861cfe04b7e18ffe
ybrenning <yannickbrenning2@gmail.com>
2022-01-20 22:54:38 +0100

    Switched adjacency list from linked list to array

--- data_structures/graphs/graph_algorithms/weighted_graphs.py
+++ data_structures/graphs/graph_algorithms/weighted_graphs.py
@@ -5,18 +5,11 @@ Implementation of weighted graphs using adjacency list
 from __future__ import annotations
 
 
-class Node:
-    def __init__(self, val: int, weight: int) -> None:
-        self.val = val
-        self.weight = weight
-        self.next: Node | None = None
-
-
 class WeightedGraph:
     def __init__(self, vertices: int, directed: bool = True) -> None:
         self.vertices = vertices
         self.directed = directed
-        self.edges: list[Node | None] = [None] * vertices
+        self.edges: list[list[(int, int)]] = [[] for _ in range(vertices)]
 
     def __str__(self) -> str:
         output = ""
@@ -24,10 +17,8 @@ class WeightedGraph:
         for i in range(0, self.vertices):
             output += f"{i}: "
 
-            curr = self.edges[i]
-            while curr is not None:
-                output += f"{curr.val} (W: {curr.weight}) "
-                curr = curr.next
+            for j in range(0, len(self.edges[i])):
+                output += f"{self.edges[i][j][0]} (W: {self.edges[i][j][1]}) "
 
             output += "\n"
 
@@ -38,12 +29,9 @@ class WeightedGraph:
 
     def has_edge(self, vertex_1: int, vertex_2: int) -> bool:
         if self.has_vertex(vertex_1) and self.has_vertex(vertex_2):
-            curr = self.edges[vertex_1]
-            while curr is not None:
-                if curr.val == vertex_2:
+            for i in range(0, len(self.edges[vertex_1])):
+                if self.edges[vertex_1][i][0] == vertex_2:
                     return True
-
-                curr = curr.next
 
         return False
 
@@ -51,44 +39,27 @@ class WeightedGraph:
         if self.has_edge(vertex_1, vertex_2):
             return False
 
-        new_node = Node(vertex_2, weight)
-        new_node.next = self.edges[vertex_1]
-        self.edges[vertex_1] = new_node
+        self.edges[vertex_1].append((vertex_2, weight))
 
         if not self.directed:
-            new_node = Node(vertex_1, weight)
-            new_node.next = self.edges[vertex_2]
-            self.edges[vertex_2] = new_node
+            self.edges[vertex_2].append((vertex_1, weight))
 
         return True
 
-    def __delete_node(self, vertex_1: int, vertex_2: int) -> bool:
-        if self.edges[vertex_1].next is None:
-            self.edges[vertex_1] = None
-        else:
-            curr = self.edges[vertex_1]
-            prev = None
-            while curr is not None and curr.val != vertex_2:
-                prev = curr
-                curr = curr.next
-
-            if curr is None:
-                return False
-
-            prev.next = curr.next
-
-            return True
+    def __delete_edge(self, vertex_1: int, vertex_2: int) -> None:
+        for i in range(0, len(self.edges[vertex_1])):
+            if self.edges[vertex_1][i][0] == vertex_2:
+                self.edges[vertex_1].pop(i)
 
     def delete_edge(self, vertex_1: int, vertex_2: int) -> bool:
-        retval = False
         if not self.has_edge(vertex_1, vertex_2):
-            return retval
+            return False
 
-        retval = self.__delete_node(vertex_1, vertex_2)
+        self.__delete_edge(vertex_1, vertex_2)
         if not self.directed:
-            retval = self.__delete_node(vertex_2, vertex_1)
+            self.__delete_edge(vertex_2, vertex_1)
 
-        return retval
+        return True
 
 
 def main():
@@ -98,10 +69,20 @@ def main():
     graph.add_edge(2, 4, 8)
     graph.add_edge(0, 1, -5)
     graph.add_edge(4, 3, 2)
+
     print(graph)
 
-    graph.delete_edge(4, 3)
-    print(graph)
+    # Alternatively:
+    other_graph = WeightedGraph(5)
+    other_graph.edges = [
+        [(1, -5)],
+        [(3, 5), (2, 1)],
+        [(4, 8)],
+        [],
+        [(3, 2)]
+    ]
+
+    print(other_graph)
 
 
 if __name__ == "__main__":
